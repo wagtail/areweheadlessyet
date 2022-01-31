7959a7622109786e8e6f3809c9c8ad9cb524af18
Tidiane Dia <atdia97@gmail.com>
2022-01-31 16:12:14 +0000

    Make mixins, variables and fonts available globally

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
+    sassOptions: {
+        // allow all scss files access to these files
+        prependData: `@import "variables.scss"; @import "mixins.scss"; @import "fonts.scss"; `,
+    },
 };
