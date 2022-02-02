72972e942afa24c8812b4baec9923db811f8bf45
Tidiane Dia <atdia97@gmail.com>
2022-02-02 16:21:40 +0000

    Transform whole CTAs components to links

--- components/BottomCTA.tsx
+++ components/BottomCTA.tsx
@@ -2,24 +2,21 @@ import Image from 'next/image';
 import styles from './BottomCTA.module.scss';
 
 export const BottomCTA = () => (
-    <div className={styles.container}>
+    <a
+        className={styles.container}
+        href="https://github.com/wagtail/wagtail/wiki/Slack"
+    >
         <div>
             <Image src="/images/hear.png" alt="" width={32} height={24} />
         </div>
         <div>
             <div className={styles.cta__text}>Hear the latest news…</div>
             <div className={styles.cta}>
-                Join{' '}
-                <a
-                    href="https://github.com/wagtail/wagtail/wiki/Slack"
-                    className={styles.link}
-                >
-                    #headless
-                </a>{' '}
-                on Slack
+                Join <strong className={styles.cta__tag}>#headless</strong> on
+                Slack
             </div>
         </div>
-    </div>
+    </a>
 );
 
 export default BottomCTA;
--- components/TopCTA.tsx
+++ components/TopCTA.tsx
@@ -2,21 +2,17 @@ import Image from 'next/image';
 import styles from './TopCTA.module.scss';
 
 export const TopCTA = () => (
-    <div className={styles.container}>
+    <a
+        className={styles.container}
+        href="https://github.com/wagtail/wagtail/wiki/Slack"
+    >
         <div>
             <Image src="/images/slack-icon.svg" alt="" width={31} height={31} />
         </div>
         <div className={styles.cta}>
-            Join{' '}
-            <a
-                href="https://github.com/wagtail/wagtail/wiki/Slack"
-                className={styles.link}
-            >
-                #headless
-            </a>{' '}
-            on Slack
+            Join <strong className={styles.cta__tag}>#headless</strong> on Slack
         </div>
-    </div>
+    </a>
 );
 
 export default TopCTA;
