5df2c55402e18f199e284f24d6c673b5cbd7ff30
Tidiane Dia <atdia97@gmail.com>
2022-02-02 16:37:59 +0000

    Rename slack-icon to slack-logo and add new line at EOF

--- components/TopCTA.tsx
+++ components/TopCTA.tsx
@@ -7,7 +7,7 @@ export const TopCTA = () => (
         href="https://github.com/wagtail/wagtail/wiki/Slack"
     >
         <div>
-            <Image src="/images/slack-icon.svg" alt="" width={31} height={31} />
+            <Image src="/images/slack-logo.svg" alt="" width={31} height={31} />
         </div>
         <div className={styles.cta}>
             Join <strong className={styles.cta__tag}>#headless</strong> on Slack
