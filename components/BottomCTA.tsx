e4501127777e0a8d51804ebcb62ce967f69397d0
Tidiane Dia <atdia97@gmail.com>
2022-02-03 12:38:44 +0000

    Define font-size variables in rem, set base font size and update styles accordingly

--- components/BottomCTA.tsx
+++ components/BottomCTA.tsx
@@ -2,21 +2,19 @@ import Image from 'next/image';
 import styles from './BottomCTA.module.scss';
 
 export const BottomCTA = () => (
-    <a
-        className={styles.container}
-        href="https://github.com/wagtail/wagtail/wiki/Slack"
-    >
-        <div>
-            <Image src="/images/hear.png" alt="" width={32} height={24} />
-        </div>
-        <div>
+    <div className={styles.container}>
+        <div className={styles.emoji}>👂</div>
+        <a
+            className={styles.link}
+            href="https://github.com/wagtail/wagtail/wiki/Slack"
+        >
             <div className={styles.cta__text}>Hear the latest news…</div>
             <div className={styles.cta}>
                 Join <strong className={styles.cta__tag}>#headless</strong> on
                 Slack
             </div>
-        </div>
-    </a>
+        </a>
+    </div>
 );
 
 export default BottomCTA;
