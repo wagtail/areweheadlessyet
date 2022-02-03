b62acc6942b7c163b402494cf1639ebebcde1f9b
Tidiane Dia <atdia97@gmail.com>
2022-02-03 10:55:48 +0000

    Add i18n configuration for lang option to be correctly set on html tag

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
+    i18n: {
+        locales: ['en'],
+        defaultLocale: 'en',
+    },
     sassOptions: {
         // allow all scss files access to these files
         prependData: `@import "/styles/variables.scss"; @import "/styles/mixins.scss"; @import "/styles/fonts.scss"; `,
