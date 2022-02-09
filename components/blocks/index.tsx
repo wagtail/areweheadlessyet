7f91d10738b773ac2f7a39a33a51789558108bc5
Tidiane Dia <atdia97@gmail.com>
2022-02-09 19:19:04 +0000

    Update types.tsx and imports

--- components/blocks/index.tsx
+++ components/blocks/index.tsx
@@ -1,5 +1,6 @@
 import NewsBlock from './news_block';
 import SectionBlock from './section_block';
 import IssuesBlock from './issues_block';
+import TopicsBlock from './topics_block';
 
-export { NewsBlock, SectionBlock, IssuesBlock };
+export { NewsBlock, SectionBlock, IssuesBlock, TopicsBlock };
