bb36dcca05f26c3bc3b3fab1e47c868ba7286352
Tidiane Dia <atdia97@gmail.com>
2022-02-10 13:31:53 +0000

    Only make 'headless' work clickable/link

--- components/FooterSlackCTA.tsx
+++ components/FooterSlackCTA.tsx
@@ -4,16 +4,16 @@ import styles from './FooterSlackCTA.module.scss';
 export const FooterSlackCTA = () => (
     <div className={styles.container}>
         <div className={styles.emoji}>👂</div>
-        <a
-            className={styles.link}
-            href="https://github.com/wagtail/wagtail/wiki/Slack"
-        >
+        <div>
             <div className={styles.cta__text}>Hear the latest news…</div>
             <div className={styles.cta}>
-                Join <strong className={styles.cta__tag}>#headless</strong> on
-                Slack
+                Join{' '}
+                <a href="https://github.com/wagtail/wagtail/wiki/Slack">
+                    <strong className={styles.cta__tag}>#headless</strong>
+                </a>{' '}
+                on Slack
             </div>
-        </a>
+        </div>
     </div>
 );
 
--- components/HeadingSlackCTA.tsx
+++ components/HeadingSlackCTA.tsx
@@ -2,17 +2,18 @@ import Image from 'next/image';
 import styles from './HeadingSlackCTA.module.scss';
 
 export const HeadingSlackCTA = () => (
-    <a
-        className={styles.container}
-        href="https://github.com/wagtail/wagtail/wiki/Slack"
-    >
+    <div className={styles.container}>
         <div>
             <Image src="/images/slack-logo.svg" alt="" width={31} height={31} />
         </div>
         <div className={styles.cta}>
-            Join <strong className={styles.cta__tag}>#headless</strong> on Slack
+            Join{' '}
+            <a href="https://github.com/wagtail/wagtail/wiki/Slack">
+                <strong className={styles.cta__tag}>#headless</strong>
+            </a>{' '}
+            on Slack
         </div>
-    </a>
+    </div>
 );
 
 export default HeadingSlackCTA;
