bedb5e37b06da3ccfb1e1fb00439da742e6da79f
Tidiane Dia <atdia97@gmail.com>
2022-02-10 11:50:28 +0000

    Use more user-friendly date format

--- components/blocks/news_block/blocks.tsx
+++ components/blocks/news_block/blocks.tsx
@@ -1,6 +1,13 @@
 import type { BlogPostValue } from './types';
 import styles from './NewsBlock.module.scss';
 
+const DateOptions = { year: 'numeric', month: 'long', day: 'numeric' } as const;
+
+const formatDate = (date: string) => {
+    const dateObject = new Date(date);
+    return new Intl.DateTimeFormat(undefined, DateOptions).format(dateObject);
+};
+
 export const PostBlock = ({
     date,
     title,
@@ -8,7 +15,7 @@ export const PostBlock = ({
     introduction,
 }: BlogPostValue) => (
     <div className={styles.card}>
-        <span className={styles.card__date}>{date}</span>
+        <span className={styles.card__date}>{formatDate(date)}</span>
         <h3 className={styles.card__title}>
             <a href={url}>{title}</a>
         </h3>
