cfc76ec5654f2aaf904d8a231d5a9f5c1686e658
Tidiane Dia <atdia97@gmail.com>
2022-02-14 14:55:35 +0000

    Put topic status color above status title instead of below

--- components/StreamField/blocks/topics_block/blocks.tsx
+++ components/StreamField/blocks/topics_block/blocks.tsx
@@ -15,10 +15,10 @@ const StatusImages = {
 
 export const TopicBlock = ({ title, status_color, introduction }: Topic) => (
     <div className={styles.card}>
-        <h3 className={styles.card__title}>{title}</h3>
-        <p className={styles.card__body}>{introduction}</p>
         <div className={styles.card__status}>
             <Image src={StatusImages[status_color]} alt="" />
         </div>
+        <h3 className={styles.card__title}>{title}</h3>
+        <p className={styles.card__body}>{introduction}</p>
     </div>
 );
